d7d99dad03a488e33d9210780d877412f2d438ba
Kazuya Arakawa <arakawa.vyw@gmail.com>
2014-05-11 14:19:07 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Html5Validators::Application.config.secret_key_base = '8f853ad790173be4b79177b8c781475bda89511f5654b7316e200b091f756d8c9e7f29192464b5f430f85fb0a3de5e464bfaf33a3a7404e83349c13e3390b87c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Html5Validators::Application.config.session_store :cookie_store, key: '_html5_validators_session'
